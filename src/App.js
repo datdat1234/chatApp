89d6c0c64e2f728c544e74018301255e7ad065a1
Deltora <deltora1st@gmail.com>
2022-11-17 23:37:28 +0700

    add login and sign-up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+// import { Navigate } from "react-router-dom";
+import { Login } from "./components/login";
+export const AppContext = React.createContext();
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContext.Provider>
+      <div id="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </div>
+    </AppContext.Provider>
   );
 }
 
---
+++ src/components/login.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+import "../styles/login.css";
+import { Signup } from "./signUp";
+
+export function Login() {
+
+  const [signUpModal, setSignUpModal] = useState(false);
+
+  return (
+      <React.Fragment>
+        <div className="login_ctn">
+          <Signup trigger={signUpModal} setTrigger={setSignUpModal} />
+          <div className="login_wrapper">
+            <h1>
+              <i class="fa-brands fa-rocketchat"></i>
+              MYCHAT
+            </h1>
+            <form method="post">
+              <div className="signup">
+                Don't have an account? <span onClick={() => {setSignUpModal(true);}}>Sign up</span>
+              </div>
+              <div className="txt_field">
+                <input type="text" required/>
+                <label>User name</label>
+                <i className="fa-regular fa-user"></i>
+              </div>
+              <div className="txt_field">
+                <input type="password" required/>
+                <label>Password</label>
+                <i className="fa-solid fa-lock"></i>
+              </div>
+              <button>Login</button>
+            </form>
+          </div>
+        </div>
+      </React.Fragment>
+  )
+}
